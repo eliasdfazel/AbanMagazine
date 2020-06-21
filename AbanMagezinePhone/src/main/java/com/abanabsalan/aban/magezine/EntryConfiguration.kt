7e69aaa9f4ac11671665f870f9f8eb386ac06b9f
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-06-21 07:50:47 -0800

    Initial Setup

---
+++ AbanMagezinePhone/src/main/java/com/abanabsalan/aban/magezine/EntryConfiguration.kt
@@ -0,0 +1,11 @@
+package com.abanabsalan.aban.magezine
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EntryConfiguration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.entry_configuration_view)
+    }
+}
