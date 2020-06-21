0349fd64842a5aa214907db8e775ca10abb48b7c
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-06-21 07:53:09 -0800

    Initial Setup

--- AbanMagezinePhone/src/main/java/com/abanabsalan/aban/magezine/EntryConfiguration.kt
+++ AbanMagezinePhone/src/main/java/com/abanabsalan/aban/magezine/EntryConfiguration.kt
@@ -1,11 +1,26 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 6/21/20 7:53 AM
+ * Last modified 6/21/20 7:52 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 package com.abanabsalan.aban.magezine
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.abanabsalan.aban.magezine.databinding.EntryConfigurationViewBinding
 
 class EntryConfiguration : AppCompatActivity() {
+
+    lateinit var entryConfigurationViewBinding: EntryConfigurationViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.entry_configuration_view)
+        entryConfigurationViewBinding = EntryConfigurationViewBinding.inflate(layoutInflater)
+        setContentView(entryConfigurationViewBinding.root)
     }
 }
