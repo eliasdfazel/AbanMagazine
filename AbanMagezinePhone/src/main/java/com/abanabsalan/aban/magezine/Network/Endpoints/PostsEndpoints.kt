6b679d7704f1eaf649192221953cf4dac4a665f6
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-06-22 16:57:49 -0800

    [Description] # Initializing
    [Phone][ChangeLog]
    # Initializing Networkings
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ AbanMagezinePhone/src/main/java/com/abanabsalan/aban/magezine/Network/Endpoints/PostsEndpoints.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 6/22/20 4:57 PM
+ * Last modified 6/22/20 4:54 PM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.abanabsalan.aban.magezine.Network.Endpoints
+
+class PostsEndpoints {
+}
