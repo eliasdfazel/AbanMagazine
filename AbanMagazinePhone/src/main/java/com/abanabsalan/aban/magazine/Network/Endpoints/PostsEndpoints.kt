899b07e2d3b02c131e61729ceccc7e25551b3be5
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-06-23 11:26:43 -0800

    [Description] #
    [Phone][ChangeLog]
    1. Configuring Endpoints
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Network/Endpoints/PostsEndpoints.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Network/Endpoints/PostsEndpoints.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 6/23/20 10:33 AM
- * Last modified 6/23/20 10:08 AM
+ * Created by Elias Fazel on 6/23/20 11:26 AM
+ * Last modified 6/23/20 11:23 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -10,5 +10,28 @@
 
 package com.abanabsalan.aban.magazine.Network.Endpoints
 
-class PostsEndpoints {
+data class PostsEndpointsFactory (
+    /**
+     * Number Of Page
+     **/
+    var numberOfPageInPosts: Int = 1,
+    /**
+     * Amount Of Posts Per Each Page
+     **/
+    var amountOfPostsToGet: Int = 3,
+    /**
+     * Sort collection by object attribute
+     **/
+    var sortByType: String = "date",
+    /**
+     * Order sort attribute ascending or descending
+     **/
+    var sortBy: String = "desc"
+)
+
+class PostsEndpoints (postsEndpointsFactory: PostsEndpointsFactory? = PostsEndpointsFactory()) {
+
+    companion object {
+        private const val PostEndpointsAddress = "https://abanabsalan.com/wp-json/wp/v2/posts?"
+    }
 }
