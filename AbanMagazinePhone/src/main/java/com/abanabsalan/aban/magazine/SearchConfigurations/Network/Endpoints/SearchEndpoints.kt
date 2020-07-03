c7e23be5e0be58e09d79aa217823b0a31ce3e15e
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-02 16:07:06 -0800

    [Description] #
    [Phone][ChangeLog]
    2. Network Connection Listener Added For HomePage.kt
    1. Network Connection Interface Added
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/SearchConfigurations/Network/Endpoints/SearchEndpoints.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 7/2/20 4:07 PM
+ * Last modified 7/2/20 4:07 PM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.abanabsalan.aban.magazine.SearchConfigurations.Network.Endpoints
+
+//http://abanabsalan.com/wp-json/wp/v2/search?search=%D9%84%D8%A8%D8%A7%D8%B3%20%D9%85%D9%86%D8%A7%D8%B3%D8%A8%20%D9%85%D8%A7%D8%AF%D8%B1%20%D8%B9%D8%B1%D9%88%D8%B3%20%D9%88%20%D9%85%D8%A7%D8%AF%D8%B1%20%D8%AF%D8%A7%D9%85%D8%A7%D8%AF
