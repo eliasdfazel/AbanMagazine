57ab8888b44ff8e0468b99e1060647b29e9d75cd
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-14 11:14:00 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/CategoriesConfigurations/UI/Extensions/SetupUserInterface.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/CategoriesConfigurations/UI/Extensions/SetupUserInterface.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel on 7/14/20 11:13 AM
- * Last modified 7/14/20 11:11 AM
+ * Last modified 7/14/20 11:13 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,19 +11,9 @@
 package com.abanabsalan.aban.magazine.CategoriesConfigurations.UI.Extensions
 
 import com.abanabsalan.aban.magazine.CategoriesConfigurations.UI.AllCategoryPosts
-import com.abanabsalan.aban.magazine.Utils.UI.FluidSlider.FluidSliderAction
-import com.abanabsalan.aban.magazine.Utils.UI.FluidSlider.FluidSliderActionInterface
 
 fun AllCategoryPosts.setupUserInterface() {
 
-  FluidSliderAction(object : FluidSliderActionInterface {
 
-      override fun fluidSliderPosition(sliderPosition: Float) {
-          super.fluidSliderPosition(sliderPosition)
-
-
-      }
-
-  }).startListener(allCategoryPostsBinding.pageFluidSlider)
 
 }
