9288a85adfcb059d5984a1317bd5977d6c787497
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-14 11:13:45 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/CategoriesConfigurations/UI/Extensions/SetupUserInterface.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/CategoriesConfigurations/UI/Extensions/SetupUserInterface.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 7/14/20 10:45 AM
- * Last modified 7/14/20 10:37 AM
+ * Created by Elias Fazel on 7/14/20 11:13 AM
+ * Last modified 7/14/20 11:11 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,18 +11,19 @@
 package com.abanabsalan.aban.magazine.CategoriesConfigurations.UI.Extensions
 
 import com.abanabsalan.aban.magazine.CategoriesConfigurations.UI.AllCategoryPosts
+import com.abanabsalan.aban.magazine.Utils.UI.FluidSlider.FluidSliderAction
+import com.abanabsalan.aban.magazine.Utils.UI.FluidSlider.FluidSliderActionInterface
 
 fun AllCategoryPosts.setupUserInterface() {
 
-    allCategoryPostsBinding.pageFluidSlider.startText = ""
-    allCategoryPostsBinding.pageFluidSlider.endText = ""
+  FluidSliderAction(object : FluidSliderActionInterface {
 
-    allCategoryPostsBinding.pageFluidSlider.positionListener = { sliderPosition ->
-        allCategoryPostsBinding.pageFluidSlider.bubbleText = "${0 + (7  * sliderPosition).toInt()}"
-    }
+      override fun fluidSliderPosition(sliderPosition: Float) {
+          super.fluidSliderPosition(sliderPosition)
 
-    allCategoryPostsBinding.pageFluidSlider.beginTrackingListener = { /* action on slider touched */ }
 
-    allCategoryPostsBinding.pageFluidSlider.endTrackingListener = { /* action on slider released */ }
+      }
+
+  }).startListener(allCategoryPostsBinding.pageFluidSlider)
 
 }
