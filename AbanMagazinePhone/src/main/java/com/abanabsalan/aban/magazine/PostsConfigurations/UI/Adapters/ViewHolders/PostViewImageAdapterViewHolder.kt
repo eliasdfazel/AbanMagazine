20b97ce01ab8052ce38f7320c01b564ccefc3722
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-18 11:20:45 -0800

    [Description] #
    [Phone][ChangeLog]
    4. Optimized Posts Items Size & Paddings
    3. Sub Title Support for Single Post Added
    2. World Wide Web News Initialized
    1. Transition for Featured Image Added
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/UI/Adapters/ViewHolders/PostViewImageAdapterViewHolder.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/UI/Adapters/ViewHolders/PostViewImageAdapterViewHolder.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 6/26/20 7:05 PM
- * Last modified 6/26/20 4:35 PM
+ * Created by Elias Fazel on 7/18/20 11:20 AM
+ * Last modified 7/18/20 11:16 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,14 +11,14 @@
 package com.abanabsalan.aban.magazine.PostsConfigurations.UI.Adapters.ViewHolders
 
 import android.view.View
-import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.post_view_content_item_image.view.*
 import net.geeksempire.loadingspin.SpinKitView
+import net.geekstools.imageview.customshapes.ShapesImage
 
 class PostViewImageAdapterViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     val rootViewItem: ConstraintLayout = view.rootViewItem
-    val postImage: ImageView = view.postImage
+    val postImage: ShapesImage = view.postImage
     val postImageLoading: SpinKitView = view.postImageLoading
 }
