a778f515d4937b7cde87a1db9ab6bf447a017bb1
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-18 09:46:26 -0800

    [Description] #
    [Phone][ChangeLog]
    2. World Wide Web News Initialized
    1. Transition for Featured Image Added
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/AllAroundWebConfigurations/Network/Endpoints/AllAroundWebEndpoints.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/AllAroundWebConfigurations/Network/Endpoints/AllAroundWebEndpoints.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 7/18/20 9:15 AM
- * Last modified 7/18/20 9:13 AM
+ * Created by Elias Fazel on 7/18/20 9:46 AM
+ * Last modified 7/18/20 9:46 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,5 +12,9 @@ package com.abanabsalan.aban.magazine.AllAroundWebConfigurations.Network.Endpoin
 
 //500 Request for Free
 //https://newsapi.org/v2/everything?q=fashion&language=en&sortBy=popularity&apiKey=1be8edb780f84184a661780a44a36bb8
-class AllAroundWebEndpoints {
+
+data class AllAroundWebEndpointsFactory(var privateKeyToNewsAPI: String)
+
+class AllAroundWebEndpoints (private val allAroundWebEndpointsFactory: AllAroundWebEndpointsFactory) {
+
 }
