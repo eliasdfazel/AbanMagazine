0f954b7de4eb2cf76e192798e563d835b03176a2
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-19 23:12:45 -0800

    [Description] #
    [Phone][ChangeLog]
    1. Preferences Initialized
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Preferences/PopupPreferencesController.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 7/19/20 11:12 PM
+ * Last modified 7/19/20 11:12 PM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.abanabsalan.aban.magazine.Preferences
+
+import androidx.appcompat.app.AppCompatActivity
+
+class PopupPreferencesController (private val appCompatActivity: AppCompatActivity) {
+
+
+}
