44527b52e738f6b8d9b07bebf5154dcb8cc5c7be
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-19 23:10:11 -0800

    [Description] #
    [Phone][ChangeLog]
    1. Preferences Initialized
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Preferences/PreferencesController.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Preferences/PreferencesController.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 7/19/20 10:54 PM
- * Last modified 7/19/20 10:40 PM
+ * Created by Elias Fazel on 7/19/20 11:10 PM
+ * Last modified 7/19/20 11:10 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -12,11 +12,9 @@ package com.abanabsalan.aban.magazine.Preferences
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.abanabsalan.aban.magazine.Preferences.UI.PreferenceFragmentUI
-import com.abanabsalan.aban.magazine.R
 import com.abanabsalan.aban.magazine.databinding.PreferencesRootViewBinding
 
-class PreferenceRootController : AppCompatActivity() {
+class PreferencesController : AppCompatActivity() {
 
     lateinit var preferencesRootViewBinding: PreferencesRootViewBinding
 
@@ -25,17 +23,14 @@ class PreferenceRootController : AppCompatActivity() {
         preferencesRootViewBinding = PreferencesRootViewBinding.inflate(layoutInflater)
         setContentView(preferencesRootViewBinding.root)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragmentPlaceholder, PreferenceFragmentUI(), "PreferencesFragment")
-            .commit()
+
 
     }
 
     override fun onPause() {
         super.onPause()
 
-        this@PreferenceRootController.finish()
+        this@PreferencesController.finish()
 
     }
 }
