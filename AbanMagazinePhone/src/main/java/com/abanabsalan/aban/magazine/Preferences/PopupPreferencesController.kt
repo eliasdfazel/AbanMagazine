70a69a2e365be995866d3bf8868df404e30087eb
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-20 18:40:13 -0800

    [Description] #
    [Phone][ChangeLog]
    3. Popup Preferences Circular Reveal Added
    2. Light/Dark Theme Toggle View Added
    1. Target Link for Images Added
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Preferences/PopupPreferencesController.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Preferences/PopupPreferencesController.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 7/20/20 3:26 PM
- * Last modified 7/20/20 3:22 PM
+ * Created by Elias Fazel on 7/20/20 6:40 PM
+ * Last modified 7/20/20 6:37 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -10,12 +10,15 @@
 
 package com.abanabsalan.aban.magazine.Preferences
 
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.abanabsalan.aban.magazine.HomePageConfigurations.UI.HomePage
 import com.abanabsalan.aban.magazine.Utils.UI.Theme.OverallTheme
 import com.abanabsalan.aban.magazine.Utils.UI.Theme.ThemeType
+import com.abanabsalan.aban.magazine.Utils.UI.Theme.toggleLightDarkThemeHomePage
 import com.abanabsalan.aban.magazine.databinding.PreferencesPopupUiViewBinding
 
-class PopupPreferencesController (private val context: AppCompatActivity, private val preferencePopupInclude: PreferencesPopupUiViewBinding) {
+class PopupPreferencesController (private val context: AppCompatActivity, private val preferencesPopupUiViewBinding: PreferencesPopupUiViewBinding) {
 
     private val overallTheme: OverallTheme by lazy {
         OverallTheme(context)
@@ -31,13 +34,13 @@ class PopupPreferencesController (private val context: AppCompatActivity, privat
 
         when (overallTheme.checkThemeLightDark()) {
             ThemeType.ThemeLight -> {
-                preferencePopupInclude.toggleTheme.apply {
+                preferencesPopupUiViewBinding.toggleTheme.apply {
                     speed = 3.33f
                     setMinAndMaxFrame(0, 7)
                 }.playAnimation()
             }
             ThemeType.ThemeDark -> {
-                preferencePopupInclude.toggleTheme.apply {
+                preferencesPopupUiViewBinding.toggleTheme.apply {
                     speed = -3.33f
                     setMinAndMaxFrame(90, 99)
                 }.playAnimation()
@@ -48,11 +51,11 @@ class PopupPreferencesController (private val context: AppCompatActivity, privat
 
     }
 
-    private fun toggleLightDarkTheme () {
+    private fun toggleLightDarkTheme() {
 
-        preferencePopupInclude.toggleTheme.setOnClickListener { view ->
+        preferencesPopupUiViewBinding.toggleTheme.setOnClickListener { view ->
 
-            preferencePopupInclude.toggleTheme.also {
+            preferencesPopupUiViewBinding.toggleTheme.also {
 
                 when (overallTheme.checkThemeLightDark()) {
                     ThemeType.ThemeLight -> {
@@ -82,6 +85,19 @@ class PopupPreferencesController (private val context: AppCompatActivity, privat
                 }
 
             }
+
+            Handler().postDelayed({
+
+                when (context) {
+                    is HomePage -> {
+
+                        toggleLightDarkThemeHomePage(context as HomePage)
+
+                    }
+                }
+
+            }, 2468)
+
 
         }
 
---
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Utils/UI/Theme/ToggleLightDarkTheme.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 7/20/20 6:40 PM
+ * Last modified 7/20/20 6:38 PM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.abanabsalan.aban.magazine.Utils.UI.Theme
+
+import com.abanabsalan.aban.magazine.HomePageConfigurations.UI.HomePage
+import com.abanabsalan.aban.magazine.R
+
+fun toggleLightDarkThemeHomePage(homePage: HomePage) {
+
+    when (homePage.overallTheme.checkThemeLightDark()) {
+        ThemeType.ThemeLight -> {
+
+            homePage.window.statusBarColor = homePage.getColor(R.color.default_color_light_transparent)
+            homePage.window.navigationBarColor = homePage.getColor(R.color.default_color_light_transparent)
+            homePage.window.decorView.setBackgroundColor(homePage.getColor(R.color.light))
+
+            homePage.homePageViewBinding.rootView.setBackgroundColor(homePage.getColor(R.color.light))
+
+            homePage.homePageViewBinding.blurViewTopBar.setOverlayColor(homePage.getColor(R.color.default_color_light_transparent))
+            homePage.homePageViewBinding.blurViewBottomBar.setOverlayColor(homePage.getColor(R.color.default_color_light_transparent))
+
+            homePage.homePageViewBinding.preferencePopupInclude.preferencesBlurView.setOverlayColor(homePage.getColor(R.color.default_color_light_transparent))
+
+        }
+        ThemeType.ThemeDark -> {
+
+            homePage.window.statusBarColor = homePage.getColor(R.color.default_color_dark_transparent)
+            homePage.window.navigationBarColor = homePage.getColor(R.color.default_color_dark_transparent)
+            homePage.window.decorView.setBackgroundColor(homePage.getColor(R.color.dark))
+
+            homePage.homePageViewBinding.rootView.setBackgroundColor(homePage.getColor(R.color.dark))
+
+            homePage.homePageViewBinding.blurViewTopBar.setOverlayColor(homePage.getColor(R.color.default_color_dark_transparent))
+            homePage.homePageViewBinding.blurViewBottomBar.setOverlayColor(homePage.getColor(R.color.default_color_dark_transparent))
+
+            homePage.homePageViewBinding.preferencePopupInclude.preferencesBlurView.setOverlayColor(homePage.getColor(R.color.default_color_dark_transparent))
+
+        }
+    }
+
+}
