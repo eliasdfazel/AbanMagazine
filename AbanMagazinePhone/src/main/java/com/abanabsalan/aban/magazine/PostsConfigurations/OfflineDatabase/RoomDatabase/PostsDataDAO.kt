9429b0d05991cfdb09e6b6b0fb394031dc4e20b3
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-25 21:50:34 -0800

    [Description] #
    [Phone][ChangeLog]
    1. Offline Posts Database Initialized {
    ~ Room Database
    ~ Json
    }
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/OfflineDatabase/RoomDatabase/PostsDataDAO.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/OfflineDatabase/RoomDatabase/PostsDataDAO.kt
@@ -1,14 +1,14 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 7/25/20 8:13 PM
- * Last modified 7/25/20 8:08 PM
+ * Created by Elias Fazel on 7/25/20 9:50 PM
+ * Last modified 7/25/20 9:28 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
 
-package com.abanabsalan.aban.magazine.PostsConfigurations.OfflineDatabase
+package com.abanabsalan.aban.magazine.PostsConfigurations.OfflineDatabase.RoomDatabase
 
 import androidx.room.*
 import io.reactivex.Flowable
--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/OfflineDatabase/RoomDatabase/PostsDataInterface.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/OfflineDatabase/RoomDatabase/PostsDataInterface.kt
@@ -1,14 +1,14 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 7/25/20 7:35 PM
- * Last modified 7/25/20 6:47 PM
+ * Created by Elias Fazel on 7/25/20 9:50 PM
+ * Last modified 7/25/20 9:28 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
 
-package com.abanabsalan.aban.magazine.PostsConfigurations.OfflineDatabase
+package com.abanabsalan.aban.magazine.PostsConfigurations.OfflineDatabase.RoomDatabase
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
