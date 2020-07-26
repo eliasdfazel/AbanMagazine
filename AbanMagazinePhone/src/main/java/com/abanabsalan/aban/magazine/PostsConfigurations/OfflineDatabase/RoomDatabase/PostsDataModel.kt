6db894cab3ab7ed84cbd0a1f7fc398b0e82fec46
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-07-25 21:51:09 -0800

    [Description] #
    [Phone][ChangeLog]
    1. Offline Posts Database Initialized {
    ~ Room Database
    ~ Json
    }
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/OfflineDatabase/RoomDatabase/PostsDataModel.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/PostsConfigurations/OfflineDatabase/RoomDatabase/PostsDataModel.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 7/25/20 9:50 PM
- * Last modified 7/25/20 9:28 PM
+ * Created by Elias Fazel on 7/25/20 9:51 PM
+ * Last modified 7/25/20 9:50 PM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
