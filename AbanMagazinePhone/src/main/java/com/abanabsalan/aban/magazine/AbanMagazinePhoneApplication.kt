285bc6d2a61303f68bea89c8c76730b02e3ac5e1
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-08-11 01:48:00 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/AbanMagazinePhoneApplication.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/AbanMagazinePhoneApplication.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 6/28/20 2:44 PM
- * Last modified 6/28/20 2:33 PM
+ * Created by Elias Fazel on 8/11/20 1:47 AM
+ * Last modified 8/11/20 1:46 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -13,6 +13,7 @@ package com.abanabsalan.aban.magazine
 import android.app.Application
 import com.abanabsalan.aban.magazine.Utils.DependencyInjections.DaggerDependencyGraph
 import com.abanabsalan.aban.magazine.Utils.DependencyInjections.DependencyGraph
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class AbanMagazinePhoneApplication : Application() {
 
@@ -22,5 +23,10 @@ class AbanMagazinePhoneApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val firebaseAnalytics = FirebaseAnalytics.getInstance(applicationContext)
+
+        firebaseAnalytics.setAnalyticsCollectionEnabled(!BuildConfig.DEBUG)
+
     }
 }
