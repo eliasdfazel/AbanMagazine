8eb2a53d9bc6f6ced5963b1107db359675af458d
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-08-13 07:12:04 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazineWatch/src/main/java/com/abanabsalan/aban/magazine/HomePageConfigurations/UI/Adapters/InstagramStoryHighlights/InstagramStoryHighlightsAdapter.kt
+++ AbanMagazineWatch/src/main/java/com/abanabsalan/aban/magazine/HomePageConfigurations/UI/Adapters/InstagramStoryHighlights/InstagramStoryHighlightsAdapter.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 8/13/20 3:11 AM
- * Last modified 8/13/20 2:52 AM
+ * Created by Elias Fazel on 8/13/20 7:12 AM
+ * Last modified 8/13/20 7:05 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -10,19 +10,25 @@
 
 package com.abanabsalan.aban.magazine.HomePageConfigurations.UI.Adapters.InstagramStoryHighlights
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.os.ResultReceiver
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.wear.widget.ConfirmationOverlay
 import com.abanabsalan.aban.magazine.HomePageConfigurations.UI.HomePage
 import com.abanabsalan.aban.magazine.InstagramConfigurations.StoryHighlights.Network.DataHolder.StoryHighlightsItemData
 import com.abanabsalan.aban.magazine.R
 import com.abanabsalan.aban.magazine.Utils.UI.Theme.OverallTheme
 import com.abanabsalan.aban.magazine.Utils.UI.Theme.ThemeType
-import com.abanabsalan.aban.magazine.WebView.BuiltInWebView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.google.android.wearable.intent.RemoteIntent
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
@@ -63,12 +69,34 @@ class InstagramStoryHighlightsAdapter (private val context: HomePage, private va
 
         instagramStoryHighlightsViewHolder.rootViewItem.setOnClickListener {
 
-            BuiltInWebView.show(
-                context = context,
-                linkToLoad = storyHighlightsItemData[position].linkToStoryHighlight,
-                gradientColorOne = context.getColor(R.color.instagramOne),
-                gradientColorTwo = context.getColor(R.color.instagramThree)
-            )
+            val resultReceiver = object : ResultReceiver(Handler()) {
+
+                override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+
+                    if (resultCode == RemoteIntent.RESULT_OK) {
+
+                        ConfirmationOverlay()
+                            .setMessage(context.getString(R.string.checkYourPhone))
+                            .setDuration(1000 * 1)
+                            .showOn(context)
+
+                    } else if (resultCode == RemoteIntent.RESULT_FAILED) {
+
+                    } else {
+
+                    }
+                }
+            }
+
+            val intentPlayStore = Intent(Intent.ACTION_VIEW)
+                .addCategory(Intent.CATEGORY_BROWSABLE)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .setData(Uri.parse(storyHighlightsItemData[position].linkToStoryHighlight))
+
+            RemoteIntent.startRemoteActivity(
+                context,
+                intentPlayStore,
+                resultReceiver)
 
         }
 
