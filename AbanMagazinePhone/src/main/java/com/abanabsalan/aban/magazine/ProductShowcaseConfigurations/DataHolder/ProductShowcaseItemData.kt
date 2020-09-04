5c7863c4727e126977cf27ed1e64c50b6270c472
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-09-04 06:24:38 -0800

    [Description] #
    [Phone][ChangeLog]
    1. Products Showcase Initialized
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/ProductShowcaseConfigurations/DataHolder/ProductShowcaseItemData.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 9/4/20 6:24 AM
+ * Last modified 9/4/20 6:17 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.abanabsalan.aban.magazine.ProductShowcaseConfigurations.DataHolder
+
+data class ProductShowcaseItemData(var nameOfProduct: String, var linkToImageProduct: String, var linkToProduct: String)
---
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/ProductShowcaseConfigurations/Endpoints/ProductShowcaseEndpoint.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2020 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 9/4/20 6:24 AM
+ * Last modified 9/4/20 6:23 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.abanabsalan.aban.magazine.ProductShowcaseConfigurations.Endpoints
+
+class ProductShowcaseEndpoint {
+
+    /**
+     * Add All Your Products Title, Image, Link To Product In A Wordpress Page Inside
+     * <p>
+     *     <a>...</a>
+     *     <a>...</a>
+     * </p>
+     * Then Replace This Link.
+     **/
+    val getProductShowcaseEndpoint = "https://abanabsalan.com/wp-json/wp/v2/pages/3111"
+
+}
