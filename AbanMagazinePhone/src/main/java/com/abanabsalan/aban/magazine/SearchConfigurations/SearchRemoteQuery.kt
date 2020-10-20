d6980e59a5fafd7c7b1f4741f5143e8d82a2a332
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-10-20 09:33:41 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/SearchConfigurations/SearchRemoteQuery.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/SearchConfigurations/SearchRemoteQuery.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2020 By Geeks Empire.
  *
- * Created by Elias Fazel on 10/20/20 9:26 AM
- * Last modified 10/20/20 9:20 AM
+ * Created by Elias Fazel on 10/20/20 9:33 AM
+ * Last modified 10/20/20 9:32 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -13,8 +13,11 @@ package com.abanabsalan.aban.magazine.SearchConfigurations
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.abanabsalan.aban.magazine.R
 import com.abanabsalan.aban.magazine.SearchConfigurations.RemoteQuery.Extensions.showPopupSearches
 import com.abanabsalan.aban.magazine.SearchConfigurations.UI.Utils.SetupSearchViewUI
+import com.abanabsalan.aban.magazine.Utils.UI.Theme.OverallTheme
+import com.abanabsalan.aban.magazine.Utils.UI.Theme.ThemeType
 import com.abanabsalan.aban.magazine.databinding.RemoteSearchViewBinding
 
 class SearchRemoteQuery : AppCompatActivity() {
@@ -25,6 +28,21 @@ class SearchRemoteQuery : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         remoteSearchViewBinding = RemoteSearchViewBinding.inflate(layoutInflater)
         setContentView(remoteSearchViewBinding.root)
+
+        val overallTheme = OverallTheme(applicationContext)
+
+        when (overallTheme.checkThemeLightDark()) {
+            ThemeType.ThemeLight -> {
+
+                remoteSearchViewBinding.searchPopupInclude.searchesBlurView.setOverlayColor(getColor(R.color.default_color_light_transparent))
+
+            }
+            ThemeType.ThemeDark -> {
+
+                remoteSearchViewBinding.searchPopupInclude.searchesBlurView.setOverlayColor(getColor(R.color.default_color_dark_transparent))
+
+            }
+        }
 
         val remoteSearchQuery = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
 
