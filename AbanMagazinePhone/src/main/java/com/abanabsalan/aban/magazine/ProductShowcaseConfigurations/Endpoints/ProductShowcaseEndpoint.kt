eee704f1b00197f3455a0c703e18f088e07c5aaf
GeeksEmpireNet <Support@GeeksEmpire.net>
2021-03-07 07:16:36 -0900

    [Description] #
    [Phone][ChangeLog]
    1. Online Store
    - All Products
    - Search Products
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Libraries Updated

--- AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/ProductShowcaseConfigurations/Endpoints/ProductShowcaseEndpoint.kt
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/ProductShowcaseConfigurations/Endpoints/ProductShowcaseEndpoint.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright © 2021 By Geeks Empire.
  *
- * Created by Elias Fazel on 2/27/21 7:25 AM
- * Last modified 2/27/21 2:52 AM
+ * Created by Elias Fazel on 3/7/21 7:16 AM
+ * Last modified 3/7/21 7:16 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -13,8 +13,8 @@ package com.abanabsalan.aban.magazine.ProductShowcaseConfigurations.Endpoints
 class ProductShowcaseEndpoint {
 
     private object Security {
-        const val ConsumerKey = "ck_6f3a98c1af6d7f8c704c65274604cd63b0d2b6d4"
-        const val ConsumerSecret = "cs_9ff6ce36823e88669359bb5e441c543be0c7fb5b"
+        const val ConsumerKey = "ck_e4cd587ec0d07dbac1138b06602e8e028a99ad43"
+        const val ConsumerSecret = "cs_55a87f5e6354700dc5fc3861d3fae730df9c4921"
     }
     /**
      * Add All Your Products Title, Image, Link To Product In A Wordpress Page Inside
