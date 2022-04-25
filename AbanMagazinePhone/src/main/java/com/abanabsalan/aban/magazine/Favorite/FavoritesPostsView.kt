eade2e93b4dd1ed99c7d4207fc7791161716dd49
GeeksEmpireNet <Support@GeeksEmpire.net>
2022-04-25 05:35:39 -0800

    [Description] #
    [Phone][ChangeLog]
    
    [Watch][ChangeLog]
    
    [Build][ChangeLog]
    # Dependencies Updated

---
+++ AbanMagazinePhone/src/main/java/com/abanabsalan/aban/magazine/Favorite/FavoritesPostsView.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2022 By Geeks Empire.
+ *
+ * Created by Elias Fazel on 4/25/22, 5:35 AM
+ * Last modified 4/25/22, 5:30 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package com.abanabsalan.aban.magazine.Favorite
+
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoritesPostsView : AppCompatActivity() {
+
+    
+
+}
